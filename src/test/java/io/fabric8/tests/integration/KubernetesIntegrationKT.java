47b8c17e10af887001f1f46c37046f5e3931f6c1
James Strachan <james.strachan@gmail.com>
2016-08-09 11:36:08 +0100

    improve system test

--- src/test/java/io/fabric8/tests/integration/KubernetesIntegrationKT.java
+++ src/test/java/io/fabric8/tests/integration/KubernetesIntegrationKT.java
@@ -16,10 +16,7 @@
 
 package io.fabric8.tests.integration;
 
-import io.fabric8.arquillian.kubernetes.Session;
 import io.fabric8.kubernetes.client.KubernetesClient;
-import io.fabric8.kubernetes.api.model.Pod;
-import org.assertj.core.api.Condition;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.junit.Test;
@@ -33,19 +30,9 @@ public class KubernetesIntegrationKT {
     @ArquillianResource
     KubernetesClient client;
 
-    @ArquillianResource
-    Session session;
-
     @Test
     public void testAppProvisionsRunningPods() throws Exception {
-        assertThat(client).pods()
-                .runningStatus()
-                .filterNamespace(session.getNamespace())
-                .haveAtLeast(1, new Condition<Pod>() {
-                    @Override
-                    public boolean matches(Pod podSchema) {
-                        return true;
-                    }
-                });
+        assertThat(client).deployments().pods().isPodReadyForPeriod();
+
     }
 }
