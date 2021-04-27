5aa5f03e3fc9c35cadb8ea01f265ae523d4f8dfc
Luigi De Masi <ldemasi@redhat.com>
2021-04-27 12:00:33 +0200

    ENTESB-16208: Migrate spring-boot-camel quickstart off Arqullian

---
+++ src/test/java/io/fabric8/tests/integration/support/KubernetesTestSetup.java
@@ -0,0 +1,40 @@
+package io.fabric8.tests.integration.support;
+
+import io.fabric8.kubernetes.client.KubernetesClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class KubernetesTestSetup {
+
+    protected static Logger LOG = LoggerFactory.getLogger(KubernetesTestSetup.class);
+
+    private KubernetesTestConfig config;
+
+    private KubernetesClient client;
+
+    public KubernetesTestSetup(KubernetesTestConfig config) {
+        this.config = config;
+        this.client = config.getClient();
+    }
+
+    public void setUp() {
+        LOG.info("Doing setup...");
+        KubernetesTestDeployer.deploy(client, config);
+        LOG.info("setup done.");
+    }
+
+    public void tearDown() {
+        LOG.info("Doing teardown...");
+        if(config.isShouldDestroyNamespace()) {
+            KubernetesTestDeployer.deleteNamespace(client, config);
+            client.rbac()
+                    .roleBindings()
+                    .inNamespace(config.getMainNamespace())
+                    .withLabels(config.getKtestLabels())
+                    .delete();
+        }else{
+            LOG.info("Nothing to do!");
+        }
+        LOG.info("Teardown done.");
+    }
+}
